30f25697e0e052906a8f4b3b7108baca604eef41
Bhavani22246 <kulkarnibhavani7@gmail.com>
2025-09-19 15:39:32 +0530

    commiting

--- prog1.c
+++ prog1.c
@@ -2,4 +2,5 @@ this line is written by shambhavi
 this line is written by Bhavani
 i am sending this line  to Bhavani
 iam using git hub application
-i am using git bash commands.. 
+i am using git bash command...
+this is my 6th line
